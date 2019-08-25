25a4fc0c522c06774273d26cc89fdf330571a030
unknown <brayden.gray98@gmail.com>
2019-08-26 09:50:39 +1200

    made config file for url and port including ajax request for both

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 let productList = $("#productList")
+let serverURL;
+let serverPort;
+
 $.ajax({
-  url: `http://localhost:3000/allProducts`,
+  url: 'config.json',
   type: 'GET',
   dataType: 'json',
-  success: function(allProducts){
-    if (allProducts.length === 0) {
-      productList.append(`There are no products`);
-    } else {
-      allProducts.map(function(products){
-        productList.append(`<li class="list-group-item d-flex justify-content-between align-items-center">${products.name}<div class="row"><button type="button" class="btn btn-info mr-2">Edit</button><button type="button" class="btn btn-danger">Remove</button></div></li>`)
-      })
-    }
+  success: function(config){
+    serverURL = config['SERVER_URL'];
+    serverPort = config['SERVER_PORT']
+    getUrlAndPort();
   },
   error: function(){
-    console.log("something went wrong");
+    console.log("cannot find config.json file, cannot run application");
   }
 })
+
+getUrlAndPort = () => {
+  $.ajax({
+    url: `${serverURL}:${serverPort}/allProducts`,
+    type: 'GET',
+    dataType: 'json',
+    success: function(allProducts){
+      if (allProducts.length === 0) {
+        productList.append(`There are no products`);
+      } else {
+        allProducts.map(function(products){
+          productList.append(`<li class="list-group-item d-flex justify-content-between align-items-center">${products.name}<div class="row"><button type="button" class="btn btn-info mr-2">Edit</button><button type="button" class="btn btn-danger">Remove</button></div></li>`)
+        })
+      }
+    },
+    error: function(){
+      console.log("something went wrong");
+    }
+  })
+}
 
 
 
